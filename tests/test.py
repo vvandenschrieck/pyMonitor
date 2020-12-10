65a155c920d8ef136a5f1287e1ef9d6b23ff2dcd
Virginie Van den Schrieck <v.vandenschrieck@gmail.com>
2020-12-10 23:26:05 +0100

    Changement - unittest => pytest

--- tests/test.py
+++ tests/test.py
@@ -1,46 +1,38 @@
-import unittest
-
 from website.website import Website, Status
 
 
-def test_mock_true(url):
+def probe_mock_true(url):
     return True
 
-def test_mock_false(url) :
+def probe_mock_false(url) :
     return False
 
-class TestingWebsite(unittest.TestCase):
+class TestWebsite():
     def test_init(self):
         site = Website("Test", "www.test.com")
-        self.assertEquals(site.status, "UNKNOWN")
+        assert site.status == "UNKNOWN"
 
     def test_probe_true(self):
         site = Website("Test", "www.test.com")
-        site.add_probe(test_mock_true)
+        site.add_probe(probe_mock_true)
         site.test()
-        self.assertEquals(site.status, "OK")
+        assert site.status == "OK"
 
     def test_probe_false(self):
         site = Website("Test", "www.test.com")
-        site.add_probe(test_mock_false)
+        site.add_probe(probe_mock_false)
         site.test()
-        self.assertEquals(site.status, "KO")
+        assert site.status == "KO"
 
     def test_multiple_probes(self):
         site = Website("Test", "www.test.com")
-        site.add_probe(test_mock_true)
-        site.add_probe(test_mock_true)
+        site.add_probe(probe_mock_true)
+        site.add_probe(probe_mock_true)
         site.test()
-        self.assertEquals(site.status, "OK")
-        site.add_probe(test_mock_false)
+        assert site.status == "OK"
+        site.add_probe(probe_mock_false)
         site.test()
-        self.assertEquals(site.status, "KO")
+        assert site.status == "KO"
 
 
-    def test_boolean(self):
-        a = True
-        b = True
-        self.assertEqual(a, b)
 
-if __name__ == '__main__':
-    unittest.main()
